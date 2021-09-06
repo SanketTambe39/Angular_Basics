bc2afb4df3c523d53f0b6f8c5773316b1c4f4b97
SanketTambe39 <sankettambe39@gmail.com>
2021-09-06 21:36:38 +0530

    ADDED: validation for input and error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,22 @@ export class AppComponent {
   title = 'Hello World';
   imgUrl ="../assets/BridgeLabz.svg";
   url ="https://www.bridgelabz.com/"
-  userName: String = "";
+  userName: string = "";
+  nameError: string = "";
   ngOnInit(): void {
     this.title = "Hello From Bridgelabz"
   }
   onClick($event){
     console.log("Save Button Is Clicked..!",$event);
     window.open(this.url,"_blank");
+  }
+  onInput($event){
+    console.log("Change Event Occured !!",$event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if(nameRegex.test(this.userName)){
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is Wrong.";
   }
 }
